199ceda32f44915243a145a590374e4e32fa8322
ChristianAlber <christianalber1997@gmail.com>
2024-01-25 10:32:01 +0100

    implement autocorrelation functions

--- pydana/harm.py
+++ pydana/harm.py
@@ -13,9 +13,17 @@ def fft(data):
 
 def compute_auto_correlation(data):
     # Compute auto correlation function of data
-    pass
+    # for the autocorrelation function, we want the overlap between
+    # the first vector at time 0 and all
+    # subsequent vectors at later times
+    # - the sum of the product of initial and
+    # subsequent vectors for each time step
+    auto_corr = np.zeros(data.shape[0], dtype="complex")
+    for i in range(data.shape[0]):
+        auto_corr[i] = np.vdot(data[0, :], data[i, :])
+    return data
 
 
 def compute_auto_corr_dft(data):
     # Compute dft of auto correlation function of data
-    pass
+    return fft(compute_auto_correlation(data))
