7e27156f8c7f972000c733f103ffb3986a92da38
ChristianAlber <christianalber1997@gmail.com>
2024-01-25 11:00:18 +0100

    implement complex conversion and strip_time fct

--- pydana/dataman.py
+++ pydana/dataman.py
@@ -1,4 +1,7 @@
-def complex_conversion():
+import numpy as np
+
+
+def complex_conversion(data):
     # This function
     # corrects the data representation: this is in
     # fact a complex matrix the real part of each
@@ -7,9 +10,13 @@ def complex_conversion():
     # each matrix column is contained in numpy array
     # column 1, 3, 5, 7, ... convert the array that
     # was read as dtype=float into a dtype=complex array
-    pass
+    data = np.ndarray.astype(data, dtype="complex")
+    data_complex = data[:, 0:-1:2] + 1j * data[:, 1::2]
+    return data_complex
 
 
-def select_column(arr, j):
-    # Selects column j of array arr
-    pass
+def strip_time(arr):
+    # Selects first column and all but first column
+    time_col = arr[:, 0]
+    data = arr[:, 1:]
+    return data, time_col
